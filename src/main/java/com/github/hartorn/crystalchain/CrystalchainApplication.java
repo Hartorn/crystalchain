317dd7a9108f71bdb351bae6af781fb87c7683cf
Hartorn <bazirehoussin@gmail.com>
2018-10-15 21:04:54 +0200

    Init Spring Boot

---
+++ src/main/java/com/github/hartorn/crystalchain/CrystalchainApplication.java
@@ -0,0 +1,12 @@
+package com.github.hartorn.crystalchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrystalchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrystalchainApplication.class, args);
+	}
+}
